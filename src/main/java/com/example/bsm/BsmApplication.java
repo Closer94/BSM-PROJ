35abd5c42ebb7002983af1bb75d2af6158e6e328
Kabsung Lee <54669100+Closer94@users.noreply.github.com>
2022-04-18 14:42:20 +0900

    test

--- src/main/java/com/example/bsm/BsmApplication.java
+++ src/main/java/com/example/bsm/BsmApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BsmApplication {
   
-  //테스트
+  //테스
   public static void main(String[] args) {
     SpringApplication.run(BsmApplication.class, args);
   } 
